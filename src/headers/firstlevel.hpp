99e20af1862f76edbcc0ff0e06e0aa4eb9dcf666
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-07-20 22:11:11 +0300

    added declarations

--- src/headers/firstlevel.hpp
+++ src/headers/firstlevel.hpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #ifndef FIRSTLEVEL_HPP
 #define FIRSTLEVEL_HPP
 
@@ -5,6 +7,10 @@ struct Part;
 
 extern Part test_ok;
 
+extern Part puya_crate;
+
 void InitFirstLvl();
+
+extern std::vector <Part> specific_parts;
 
 #endif
