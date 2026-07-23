276f3393847b91fc84601e36456add3e67f1e551
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-07-23 13:29:02 +0300

    added more general parts
    hi

--- src/headers/generallvl.hpp
+++ src/headers/generallvl.hpp
@@ -18,6 +18,35 @@ typedef struct Part {
 
 } Part;
 
+//this struct will be used to kill the player, its hitbox is a bit bigger
+typedef struct KillPart {
 
+    float x;
+    float y;
+
+    float sizeX;
+    float sizeY;
+
+    Texture tex;
+
+    Rectangle hitbox = {x, y, sizeX + 5, sizeY + 5};
+
+} KillPart;
+
+//the part as a checkpoint
+//has a texture and a predefined size
+typedef struct checkpoint {
+
+    float x;
+    float y;
+
+    float sizeX = 50;
+    float sizeY = 20;
+
+    Texture tex = LoadTexture("resources/map_part/checkpoint_50x20.png");
+
+    Rectangle hitbox = {x, y, sizeX, sizeY + 5};
+
+} checkpoint;
 
 #endif
